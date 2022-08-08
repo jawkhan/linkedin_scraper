ec4f19ce1adb858a06b0d7ee398e90aa9d9204c7
Amr Amr <67161059+amrraedamr@users.noreply.github.com>
2022-08-08 15:37:25 +0300

    Update objects.py

--- linkedin_scraper/objects.py
+++ linkedin_scraper/objects.py
@@ -1,3 +1,12 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .objects import Experience, Education, Scraper, Interest, Accomplishment, Contact
+import os
+from linkedin_scraper import selectors
+
 from dataclasses import dataclass
 
 from selenium.webdriver import Chrome
