dc8c9ddbf4d2e90e40814396ac56bc0e048d55dc
Amr Amr <67161059+amrraedamr@users.noreply.github.com>
2022-08-08 16:38:43 +0300

    Update person.py

--- linkedin_scraper/person.py
+++ linkedin_scraper/person.py
@@ -119,21 +119,21 @@ class Person(Scraper):
 
         # get about
         try:
-            see_more = WebDriverWait(driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
-                EC.presence_of_element_located(
-                    (
-                        By.XPATH,
-                        "//*[@class='lt-line-clamp__more']",
-                    )
-                )
-            )
-            driver.execute_script("arguments[0].click();", see_more)
+#             see_more = WebDriverWait(driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
+#                 EC.presence_of_element_located(
+#                     (
+#                         By.XPATH,
+#                         "//*[@class='lt-line-clamp__more']",
+#                     )
+#                 )
+#             )
+#             driver.execute_script("arguments[0].click();", see_more)
 
             about = WebDriverWait(driver, self.__WAIT_FOR_ELEMENT_TIMEOUT).until(
                 EC.presence_of_element_located(
                     (
                         By.XPATH,
-                        "//*[@class='lt-line-clamp__raw-line']",
+                        "//*[@id='ember76']/div[3]/div/div/div/span[1]",
                     )
                 )
             )
